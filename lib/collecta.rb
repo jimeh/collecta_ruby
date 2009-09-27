6cc2d210ed9983fef7b38af31687c0648b128dcd
Jim Myhrberg <contact@jimeh.me>
2009-09-27 20:26:01 +0300

    the core stuff, almost complete

---
+++ lib/collecta.rb
@@ -0,0 +1,84 @@
+require "rubygems"
+require "net/http"
+require "uri"
+require "cgi"
+require "json"
+require "yaml"
+# require "xml" # gem libxml-ruby >= 0.8.3
+
+
+class Collecta
+  
+  @@api_key = nil
+  @@api_url = "http://api.collecta.com/search"
+  
+  def self.api_key
+    @@api_key
+  end
+  
+  def self.api_key=(key)
+    @@api_key = key
+  end
+  
+  def self.search(q = nil, params = {})
+    if !q.nil? && q != ""
+      params[:q] = q
+      return self.request(params)
+    end
+    return false
+  end
+  
+  def self.request(options = {})
+    params = {}
+    params[:api_key] = @@api_key if options[:api_key].nil?
+    params[:format]  = (!options[:format].nil?) ? options[:format] : "hash"
+    valid_params = [:q, :rpp, :page, :since_id, :callback, :category, :exclude]
+    valid_params.each do |param|
+      params[param] = options[param] if !options[param].nil?
+    end
+    request_url = self.build_url(params.clone)
+    if request_url
+      response = Net::HTTP.get(URI.parse(request_url))
+      case params[:format]
+      when "hash"
+        return JSON.parse(response)
+      else
+        return response
+      end
+    end
+    return false
+  end
+  
+  def self.build_url(params)
+    if !params[:q].nil? && !params[:api_key].nil?
+      # show specific categories
+      if params[:category].is_a?(Array) && params[:category].size > 0
+        params[:q] << params[:category].map { |item| " category:#{item}"}.join(" OR")
+      elsif params[:category].is_a?(String)
+        params[:q] << params[:category] && params[:category] != ""
+      end
+      params.delete(:category)
+      # exclude specific categories
+      if params[:exclude].is_a?(Array) && params[:exclude].size > 0
+        params[:q] << params[:exclude].map { |item| " -category:#{item}"}.join(" OR")
+      elsif params[:exclude].is_a?(String)
+        params[:q] << params[:exclude] && params[:exclude] != ""
+      end
+      params.delete(:exclude)
+      return @@api_url + self.build_query(params)
+    end
+    return false
+  end
+  
+  def self.build_query(params, first = true)
+    query = (first) ? "?" : ""
+    params[:format] = "json" if params[:format] == "hash"
+    return query << params.map { |name, value| "#{name}=" + CGI::escape(value).gsub("+", "%20") }.join("&")
+  end
+  
+end
+
+Collecta.api_key = "fb803590121d55976a9329031494d72b"
+puts Collecta.search("iphone", :format => "hash")
+
+
