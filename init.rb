a94477b921e1bf75b0e4a0864b78ed2cd30ea255
Jim Myhrberg <contact@jimeh.me>
2009-10-04 03:25:04 +0300

    load collecta api key from config/collecta.yml if available

--- init.rb
+++ init.rb
@@ -1,4 +1,11 @@
 
 if defined? Rails
-    require 'collecta'
+  require 'collecta'
+  config_file = File.join(RAILS_ROOT, "config", "collecta.yml")
+  if File.exist?(config_file)
+    config = YAML.load_file(config_file)
+    if !config[RAILS_ENV.to_s].nil? && !config[RAILS_ENV.to_s]["api_key"].nil?
+      Collecta.api_key = config[RAILS_ENV.to_s]["api_key"]
+    end
+  end
 end
